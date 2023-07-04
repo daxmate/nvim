bd16c054128e386a87008776db6f2edcc32d1b4b
dax <dax26d@gmail.com>
2023-07-04 17:51:40 +0800

    add settings of lsp for lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,12 +3,12 @@ require"lspconfig".lua_ls.setup { -- lua
         Lua = {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT"
+                version = "Lua"
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
                 globals = {
-                    "vim", "string", "table", "require", "Candidate", "yield", "local"
+                    "vim", "string", "table", "require", "Candidate", "yield", "local", "tex"
                 }
             },
             workspace = {
