02f62581dc961dea0c60f48485bb2145ff932c12
dax <dax26d@gmail.com>
2023-09-29 16:50:57 +0800

    remove treesitter latex parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = {"python", "latex", "lua", "cpp", "vim"},
+    ensure_installed = {"python", "lua", "cpp", "vim"},
     highlight = {enable = true, additional_vim_regex_highlighting = false},
     indent = {enable = true}
 }
