5218ad5cbe4241d5086806da1e9eae63574579f3
dax <dax26d@gmail.com>
2024-01-02 10:10:54 +0800

    add markdown to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = {"python", "lua", "cpp", "vim"},
+    ensure_installed = {"python", "lua", "cpp", "vim", "markdown"},
     highlight = {enable = true, additional_vim_regex_highlighting = false},
     indent = {enable = true}
 }
