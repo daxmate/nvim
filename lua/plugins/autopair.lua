8e245cb8297c9cda82bbc4edf779d07be1a6e082
dax <dax26d@gmail.com>
2025-01-06 20:31:25 +0800

    增加autopairs插件中LaTeX文档中行内公式环境$$中跳出的功能

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,5 +1,13 @@
 local Rule = require('nvim-autopairs.rule')
 local npairs = require('nvim-autopairs')
-npairs.setup{}
+npairs.setup {}
 
-npairs.add_rule(Rule("$", "$", "tex"))
+-- 设置 $ 符号的配对规则，并添加跳出功能
+npairs.add_rule(Rule("$", "$"):with_move(function()
+    local col = vim.fn.col('.')
+    local line = vim.fn.line('.')
+    local curr_char = vim.fn.getline(line):sub(col, col)
+    -- 如果光标后面是 $，按下 $ 就跳出配对
+    if curr_char == "$" then return true end
+    return false
+end))
