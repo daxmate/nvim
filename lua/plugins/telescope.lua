6aec19f66a093b4e3cb587aaf1543290a61580f5
dax <dax26d@gmail.com>
2026-02-04 20:49:31 +0800

    去除telescope的版本限制

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- plugins/telescope.lua
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
+	-- tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-live-grep-args.nvim",
