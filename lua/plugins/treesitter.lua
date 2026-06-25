4a04bacb25b81c48f0c146f98ed9ba6599d1975a
dax <dax26d@gmail.com>
2026-06-25 14:44:20 +0800

    fix(treesitter): switch to main branch, fix textobjects dependency and Neovim 0.12 query compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,22 +2,40 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
-		lazy = false,
 		config = function()
 			require("nvim-treesitter").setup({
 				ensure_installed = { "latex", "python", "lua", "cpp", "vim", "markdown", "bash" },
 				highlight = {
 					enable = true,
-					disable = { "latex" },
 				},
 				indent = { enable = true },
 			})
 			vim.treesitter.language.register("bash", "zsh")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		lazy = true,
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+					},
+				},
+			})
 		end,
 	},
 }
